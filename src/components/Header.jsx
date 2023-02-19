03c22231af925c9ae89bee4ecf5f5d453eb0bca0
Avanish Kumar Bind <you@example.com>
2023-02-20 03:42:58 +0530

    reinitialized files

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+
+function Header() {
+  return (
+    <header>
+      <h1><MenuBookIcon/> Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
